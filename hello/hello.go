d26dc508ca437e3d774982762599c1245fe6509e
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-29 15:20:10 -0400

    Integrate rsc.io/quote package into hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,23 @@
-// This code prints "Hello, World!" to the console
-package main
+// // This code prints "Hello, World!" to the console
+// package main
 
-import "fmt" // import fmt package for formatting output
+// import "fmt" // import fmt package for formatting output
 
-func main() { // main function, entry point of program
-    fmt.Println("Hello, World!") // print out "Hello, World!" to the console
+// func main() { // main function, entry point of program
+//     fmt.Println("Hello, World!") // print out "Hello, World!" to the console
+// }
+
+
+
+
+// This program imports the rsc.io/quote package and prints a quote from it using the fmt package
+package main 
+
+import (
+	"fmt" // Package fmt implements formatted I/O with functions analogous to C's printf and scanf
+	"rsc.io/quote" // Package quote provides quotes from the Go authors and contributors.
+)
+
+func main() {
+	fmt.Println(quote.Go()) // Prints a quote from the Go authors and contributors
 }
