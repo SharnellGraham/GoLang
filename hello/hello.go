795569a84ebae99b893a0ca7b50f448d196d8310
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-29 16:06:37 -0400

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -23,6 +23,37 @@
 // }
 
 
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+
+//     "example.com/greetings"
+// )
+
+// func main() {
+//     // Set properties of the predefined Logger, including
+//     // the log entry prefix and a flag to disable printing
+//     // the time, source file, and line number.
+//     log.SetPrefix("greetings: ")
+//     log.SetFlags(0)
+
+//     // Request a greeting message.
+//     message, err := greetings.Hello("Gladys")
+//     // If an error was returned, print it to the console and
+//     // exit the program.
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+
+//     // If no error was returned, print the returned message
+//     // to the console.
+//     fmt.Println(message)
+// }
+
+
+
 package main
 
 import (
@@ -39,15 +70,15 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("Gladys")
-    // If an error was returned, print it to the console and
-    // exit the program.
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
     if err != nil {
         log.Fatal(err)
     }
-
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
