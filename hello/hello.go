e7ab73f5557ffdb0d51afedba368c1ba1d3aa1c6
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-29 15:13:53 -0400

    Hello word starter go code

---
+++ hello/hello.go
@@ -0,0 +1,8 @@
+// This code prints "Hello, World!" to the console
+package main
+
+import "fmt" // import fmt package for formatting output
+
+func main() { // main function, entry point of program
+    fmt.Println("Hello, World!") // print out "Hello, World!" to the console
+}
