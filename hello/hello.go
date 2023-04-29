792b930dc8c5ee66a03f2894b8562cee83479cc4
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-29 15:40:27 -0400

    Call your code from another module

--- hello/hello.go
+++ hello/hello.go
@@ -10,14 +10,28 @@
 
 
 
-// This program imports the rsc.io/quote package and prints a quote from it using the fmt package
-package main 
+// // This program imports the rsc.io/quote package and prints a quote from it using the fmt package
+// package main 
+
+// import (
+// 	"fmt" // Package fmt implements formatted I/O with functions analogous to C's printf and scanf
+// 	"rsc.io/quote" // Package quote provides quotes from the Go authors and contributors.
+// )
+
+// func main() {
+// 	fmt.Println(quote.Go()) // Prints a quote from the Go authors and contributors
+// }
+
+
+package main
 
 import (
-	"fmt" // Package fmt implements formatted I/O with functions analogous to C's printf and scanf
-	"rsc.io/quote" // Package quote provides quotes from the Go authors and contributors.
+    "fmt"
+    "example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go()) // Prints a quote from the Go authors and contributors
+    // Get a greeting message and print it.
+    message := greetings.Hello("Gladys")
+    fmt.Println(message)
 }
